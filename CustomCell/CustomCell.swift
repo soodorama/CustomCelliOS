8499bceccf087e77c378178a31318ca92b8b466b
Neil Sood <neilsood@Neils-MacBook-Pro.local>
2018-09-11 21:37:59 -0700

    custom cell app

---
+++ CustomCell/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  CustomCell
+//
+//  Created by Neil Sood on 9/11/18.
+//  Copyright © 2018 Neil Sood. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+    @IBOutlet weak var leftButton: UIButton!
+    
+    @IBOutlet weak var rightLabel: UILabel!
+    
+    
+}
---
+++ CustomCell/CustomCellsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  CustomCell
+//
+//  Created by Neil Sood on 9/11/18.
+//  Copyright © 2018 Neil Sood. All rights reserved.
+//
+
+import UIKit
+
+class CustomCellsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var nums = [1, 90, 32, 23, 9, 12]
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomCell
+        cell.rightLabel.text = "\(nums[indexPath.row])"
+        if nums[indexPath.row] > 24 {
+            cell.leftButton.backgroundColor = UIColor.green
+        } else {
+            cell.leftButton.backgroundColor = UIColor.red
+        }
+        // return cell so that Table View knows what to draw in each row
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nums.count
+    }
+
+}
+
